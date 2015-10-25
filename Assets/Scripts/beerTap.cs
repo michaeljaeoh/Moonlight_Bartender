630e25ee90a146c4ef9e25fd956332433e2ccd2d
michaeljaeoh <mjoh@uci.edu>
2015-10-25 12:33:49 -0700

    Added BeerTap Animation

---
+++ Assets/Scripts/beerTap.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class beerTap : MonoBehaviour {
+
+    Animator animator;
+
+    bool pouring = false;
+    float pourTime = 0;
+    float pourDelay = 1.5f;
+
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0) && !pouring)
+        {
+            pourTime = pourDelay;
+            pouring = true;
+            animator.SetTrigger("tapON");
+        }
+        pourTime -= Time.deltaTime;
+        if (pourTime <= 0 && pouring)
+        {
+            animator.SetTrigger("tapOFF");
+            pouring = false;
+        }
+
+	}
+}
