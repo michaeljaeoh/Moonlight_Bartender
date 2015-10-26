d38fb0e6059ee10e59175a5f6776db97503d26e8
michaeljaeoh <mjoh@uci.edu>
2015-10-25 17:42:50 -0700

    Got triggers working

---
+++ Assets/Scripts/beerGlass.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class beerGlass : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            var mousePos = Input.mousePosition;
+            var wantedPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10));
+            transform.position = wantedPos;
+        }
+
+	}
+
+    void OnTriggerEnter2D(Collider2D collider2d)
+    {
+        //if (collider2d.tag == "BeerTap")
+        //{
+            print("waddddddduppppp");
+       //     Destroy(this.gameObject);
+        //}
+    }
+    
+}
--- Assets/Scripts/beerTap.cs
+++ Assets/Scripts/beerTap.cs
@@ -4,33 +4,58 @@ using System.Collections;
 public class beerTap : MonoBehaviour {
 
     Animator animator;
+    Transform transform;
 
+    bool glassPresent = false;
     bool pouring = false;
     float pourTime = 0;
     float pourDelay = 1.5f;
-
+    bool leftMouseButton;
+    Collider2D glass;
 
 	// Use this for initialization
 	void Start () {
         animator = GetComponent<Animator>();
+
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetMouseButtonDown(0) && !pouring)
+        if (Input.GetMouseButtonUp(0) && glassPresent)
         {
             pourTime = pourDelay;
             pouring = true;
             animator.SetTrigger("tapON");
+            Destroy(glass.gameObject);
         }
+        
         pourTime -= Time.deltaTime;
         if (pourTime <= 0 && pouring)
         {
             animator.SetTrigger("tapOFF");
+            glassPresent = false;
             pouring = false;
         }
-
+        
 	}
+
+    void OnTriggerEnter2D(Collider2D collider2d)
+    {
+        if (collider2d.tag == "BeerGlass")
+        {
+            glassPresent = true;
+            glass = collider2d;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider2d)
+    {
+        if (collider2d.tag == "BeerGlass")
+        {
+            glassPresent = false;
+            glass = null;
+        }
+    }
 }
