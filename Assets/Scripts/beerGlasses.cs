d533baa2218f4dacd668164a25e349adb320c7c5
Ford Tang <FordTang@gmail.com>
2015-11-24 00:16:12 -0800

    Added Fryer object, animation and sprites

---
+++ Assets/Scripts/beerGlasses.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class beerGlasses : MonoBehaviour {
+    private bool mouseOver;
+    public world worldInfo;
+    public beerGlass beerglass;
+
+	// Use this for initialization
+	void Start () {
+        Physics.queriesHitTriggers = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnMouseOver() {
+        if (!worldInfo.getMouseBusy() && Input.GetMouseButtonDown(0))
+        {
+            worldInfo.setMouseBusy();
+            Instantiate(beerglass);
+        }
+    }
+}
