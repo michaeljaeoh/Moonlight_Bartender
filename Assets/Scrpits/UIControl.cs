66a702bacfced18645f194cfc412db3529628d49
ray80929 <ray80929@hotmail.com>
2015-11-25 15:12:15 -0800

    chia gay
    chiaed

---
+++ Assets/Scrpits/UIControl.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIControl : MonoBehaviour {
+
+	public void ChangeScene(string sceneName)
+	{
+		Application.LoadLevel (sceneName);
+	}
+	
+
+}
