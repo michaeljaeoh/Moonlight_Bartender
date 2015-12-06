e52119b038337c3dacc2c0a842008216c6179a84
michaeljaeoh <mjoh@uci.edu>
2015-12-05 16:41:41 -0800

    Shaker In-Progress
    Shaker Script

---
+++ Assets/Scripts/Shaker.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using System;
+
+public class Shaker : BarSpawner {
+
+    private bool alcPresent, validItem;
+    Collider2D mixItem;
+    public BarItem martini;
+    public BarItem mojito;
+
+    public List<string> added;
+
+	// Use this for initialization
+	void Start () {
+	    added = new List<string>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (alcPresent && Input.GetMouseButtonUp(0))
+        {
+            added.Add(mixItem.tag);
+        }
+        else if (validItem && Input.GetMouseButtonUp(0))
+        {
+            added.Add(mixItem.tag);
+        }
+        else if (added.Count > 1)
+        {
+            createDrink();
+        }
+        
+	}
+
+    void createDrink()
+    {
+        if (added.Contains("Alcohol") && added.Contains("Olive"))
+        {
+            Instantiate(martini);
+        }
+        else if (added.Contains("Alcohol") && added.Contains("Mint"))
+        {
+            Instantiate(mojito);
+        }
+        else
+        {
+
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider2d)
+    {
+        if (collider2d.tag == "Alcohol")
+        {
+            mixItem = collider2d;
+            alcPresent = true;
+        }
+        if (!validItem && (collider2d.tag == "Olive" || collider2d.tag == "Mint"))
+        {
+            mixItem = collider2d;
+            validItem = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider2d)
+    {
+        if (collider2d.tag == "Alcohol")
+        {
+            mixItem = null;
+            alcPresent = false;
+        }
+        if (collider2d.tag == "Olive" || collider2d.tag == "Mint")
+        {
+            mixItem = null;
+            validItem = false;
+        }
+    }
+}
