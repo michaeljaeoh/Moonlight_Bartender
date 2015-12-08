2bebd961ed7c3d2e90f04696f4e094d75144f569
ray80929 <ray80929@hotmail.com>
2015-12-07 16:02:19 -0800

    shop
    123

---
+++ Assets/money.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class money : MonoBehaviour {
+
+	public static Text txt;
+	private int Money = 0;
+	void Awake()
+	{
+		txt  = GetComponent<Text>();
+		//txt.text = "hi";
+	}
+	void Start (){
+		//GameObject.Find("CountText").GetComponent<Text>();
+		//txt  = GameObject.Find ("money display/Text").GetComponent<Text>;
+		//txt.text = "hi";
+		//txt  = transform.Find ("money display/Text").GetComponent<Text>();
+		//txt.text = "hi";
+	}
+
+	void Update() {
+		Money += 100;
+		txt.text = "$ " + Money.ToString();
+	}
+
+}
